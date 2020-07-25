baed664a404e6d988be87325fb12160804a7716f
Jason McDonald <jmcdon31@gmail.com>
2020-07-25 11:17:45 -0600

    forgot to add files

---
+++ sys_setup.sh
@@ -0,0 +1,30 @@
+#install docker
+	sudo apt-get update && \
+
+	sudo apt-get install \
+    	apt-transport-https \
+    	ca-certificates \
+    	curl \
+    	gnupg-agent \
+    	software-properties-common && \
+
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+
+	sudo apt-get install docker-ce docker-ce-cli containerd.io  && \
+
+
+#Download Docker Images
+	sudo docker build https://github.com/jmcdon31/Dockerfiles.git#master:devenv -t devenv && \
+
+#Setup Aliases
+	echo  "alias edit='sudo docker run -it  -v $PWD:/root/${PWD##*/} devenv'" >> ~/.bash_aliases && \
+
+#install gnome tweak tool
+	sudo apt-get install gnome-tweak-tool
+	
+	#####MANUALLY set caps to ctrl#####
+
+	
+
+	
+	
